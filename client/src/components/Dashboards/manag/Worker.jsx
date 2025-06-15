b979a66ab120758ce2fdefe4370eeaf3be88fe71
yitzchak <yitzchakrothman@gmail.com>
2025-06-15 15:40:51 +0300

    fixed ui for responsive grid layout and css for buttton

--- client/src/components/Dashboards/manag/Worker.jsx
+++ client/src/components/Dashboards/manag/Worker.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import AssignedCasesList from './AssignedCasesList';
-
+import styles from '../../../styles/ManagerLayout.module.css';
 export default function Worker({ worker }) {
   const [showPopup, setShowPopup] = useState(false);
 
@@ -9,20 +9,22 @@ export default function Worker({ worker }) {
   };
 
   return (
-    <div>
+    < >
       <h2>{worker.firstName} {worker.lastName}</h2>
       <p>Region: {worker.region}</p>
       <p>
         Specialties: {Array.isArray(worker.specialties) ? worker.specialties.join(', ') : worker.specialties}
       </p>
       <p><strong>Case Load: {worker?.cases?.length}</strong></p>
-      <button onClick={handleViewCases}>View assigned cases</button>
+      <button onClick={handleViewCases}
+      className={styles.viewCasesButton}
+      >Assigned cases</button>
       <AssignedCasesList
         worker={worker}
         open={showPopup}
         onClose={() => setShowPopup(false)}
         cases={worker.cases || []}
       />
-    </div>
+    </>
   );
 }
